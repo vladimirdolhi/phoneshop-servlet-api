556cec5a620f3dcc894c271b19d26861c15c8980
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2022-12-30 19:44:30 +0300

    implement ArrayListProductDaoTest using junit4

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -1,12 +1,16 @@
 package com.es.phoneshop.model.product;
 
+import com.es.phoneshop.exception.ProductDaoException;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.List;
 
-public class ArrayListProductDaoTest
-{
+import static org.junit.Assert.*;
+
+public class ArrayListProductDaoTest {
     private ProductDao productDao;
 
     @Before
@@ -16,6 +20,76 @@ public class ArrayListProductDaoTest
 
     @Test
     public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+        assertFalse(productDao.findProducts().isEmpty());
     }
+
+    @Test
+    public void testSaveNewProduct() throws ProductDaoException {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product(19L, "test", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        Product foundProduct = productDao.getProduct(product.getId());
+        assertNotNull(foundProduct);
+        assertEquals("test", foundProduct.getCode());
+        assertFalse(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testFindProductWithZeroStock() throws ProductDaoException {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product(23L, "test", "Samsung Galaxy S", new BigDecimal(100), usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        List<Product> productList = productDao.findProducts();
+        assertFalse(productList.contains(product));
+    }
+
+    @Test
+    public void testFindProductWithNullPrice() throws ProductDaoException {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product(24L, "test", "Samsung Galaxy S", null, usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        List<Product> productList = productDao.findProducts();
+        assertFalse(productList.contains(product));
+    }
+
+
+    @Test
+    public void testDeleteProduct() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product(25L, "testProductToDelete", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+
+        try {
+            productDao.save(product);
+            productDao.delete(25L);
+            Product foundProduct = productDao.getProduct(25L);
+            fail("Expected ProductDaoException");
+        } catch (ProductDaoException e) {
+            assertEquals(e.getMessage(), new ProductDaoException("Product with id 25 not found").getMessage());
+        }
+
+    }
+
+    @Test
+    public void testSaveProductWithNullId() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product(null, "testProductToDelete", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+
+        try {
+            productDao.save(product);
+            fail("Expected ProductDaoException");
+        } catch (ProductDaoException e) {
+            assertEquals(e.getMessage(), new ProductDaoException("Product id is not initialized").getMessage());
+        }
+
+    }
+
+    @Test
+    public void testGetProduct() throws ProductDaoException {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product(31L,"testProductToGet", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+
+        productDao.save(product);
+        assertEquals(productDao.getProduct(product.getId()), product);
+    }
+
 }
